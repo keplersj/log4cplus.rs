cea20fc92f4657815394162653afe6c204f58803
Kepler Sticka-Jones <kepler@stickajones.org>
2025-01-04 01:28:07 -0700

    refactor(yak_shaver): use construction Razor finder

--- log-log4cplus/examples/yak_shaver.rs
+++ log-log4cplus/examples/yak_shaver.rs
@@ -5,6 +5,19 @@ use rand::Rng;
 
 struct Razor;
 
+impl Razor {
+    fn find() -> Option<Self> {
+        trace!("Locating a razor");
+        if rand::thread_rng().gen_ratio(1, 20) {
+            debug!("You got a Nat 20!");
+            Some(Razor)
+        } else {
+            error!("Reroll Required");
+            None
+        }
+    }
+}
+
 impl Display for Razor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Razor")
@@ -19,29 +32,18 @@ impl Yak {
     }
 }
 
-fn find_a_razor() -> Result<Razor, String> {
-    trace!("Locating a razor");
-    if rand::thread_rng().gen_ratio(1, 20) {
-        debug!("You got a Nat 20!");
-        Ok(Razor)
-    } else {
-        error!("Reroll Required");
-        Err("Razor not found".to_string())
-    }
-}
-
 pub fn shave_the_yak(yak: &mut Yak) {
     trace!("Commencing yak shaving");
 
     loop {
-        match find_a_razor() {
-            Ok(razor) => {
+        match Razor::find() {
+            Some(razor) => {
                 info!("Razor located: {razor}");
                 yak.shave(razor);
                 break;
             }
-            Err(err) => {
-                warn!("Unable to locate a razor: {err}, retrying");
+            None => {
+                warn!("Unable to locate a razor, retrying");
             }
         }
     }
